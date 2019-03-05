883f894960ea65ab8e79219be94337ea6ab75796
Jamie Thomson <jamie.thomson@dunnhumby.com>
2019-03-05 10:17:03 +0000

    network/subnet name changes

--- network.tf
+++ network.tf
@@ -1,11 +1,11 @@
-resource "google_compute_network" "democratising-dataproc-network" {
-  name                    = "democratising-dataproc-network"
+resource "google_compute_network" "network" {
+  name                    = "${var.project}-network"
   auto_create_subnetworks = false
 }
 
-resource "google_compute_subnetwork" "democratising-dataproc-subnet" {
-  name          = "${google_compute_network.democratising-dataproc-network.name}-subnet"
+resource "google_compute_subnetwork" "subnet" {
+  name          = "${google_compute_network.network.name}-subnet"
   region        = "${var.region}"
   ip_cidr_range = "10.2.0.0/16"
-  network       = "${google_compute_network.democratising-dataproc-network.self_link}"
+  network       = "${google_compute_network.network.self_link}"
 }
